b4a956a0d9b5788193421989fe5dd53106af1932
Raimond Spekking <raymond@users.mediawiki.org>
2012-02-23 19:45:08 +0000

    Localisation updates for extension messages from translatewiki.net Core skipped today, conflicts with dupe messages... Will report on CR soon

--- PagedTiffHandler.i18n.php
+++ PagedTiffHandler.i18n.php
@@ -537,6 +537,7 @@ dat mer kein Breefmarkebelldsche ußrääschne künne.',
  */
 $messages['lb'] = array(
 	'tiff-desc' => 'Programm deen et erméiglech TIFF-Fichieren als Bild ze kucken',
+	'tiff_no_metadata' => 'Meta-Informatiounen aus dem TIFF kënnen net gelies ginn',
 	'tiff_page_error' => "D'Nummer vun der Säit ass net am Beräich",
 	'tiff_file_too_large' => 'Den eropgeluedene Fichier ass ze grouss a gouf net akzeptéiert.',
 	'tiff_out_of_service' => 'Den eropgeluedene Fichier konnt net verschafft ginn. ImageMagick ass net disponibel.',
