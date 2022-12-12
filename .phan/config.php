b025db283b853d694b532e840ae1e9760741997e
Umherirrender <umherirrender_de.wp@web.de>
2022-12-12 20:18:31 +0100

    build: Remove unneeded phan suppression
    Follow-Up: If25bbf071527579fa310825e84264422c8cd75a4
    Change-Id: I6cd9acbaeafe9a62668dbdf0e1fb095f285e767f

--- .phan/config.php
+++ .phan/config.php
@@ -1,8 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-
-// Due to creation of File->tiffMetaArray/tiffImage property
-$cfg['suppress_issue_types'][] = 'PhanUndeclaredProperty';
-
-return $cfg;
+return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
