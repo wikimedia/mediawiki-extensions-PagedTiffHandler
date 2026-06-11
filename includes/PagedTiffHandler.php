f4feac79040e6bd7f1626fa5a578f43d18ad54bc
Sam Reed <sam@reedyboy.net>
2026-06-11 23:26:02 +0100

    PagedTiffHandler: Handle missing page_count from metadata
    bug: T422532
    Change-Id: I6c6aace72569ea793a98c650db0e77526d1f8cf6
    (cherry picked from commit 391fc2542de7735cbbc7d6313fa69cb2b25ca02d)

--- includes/PagedTiffHandler.php
+++ includes/PagedTiffHandler.php
@@ -495,7 +495,7 @@ class PagedTiffHandler extends TransformationalImageHandler {
 			return 1;
 		}
 
-		return intval( $data['page_count'] );
+		return intval( $data['page_count'] ?? 1 );
 	}
 
 	/**
